3082e368a49f33f0de302ad9b74aac464a5ab21d
DESKTOP-F3TNLC7\liuxy <liuxianyun5099@163.com>
2016-12-31 10:14:53 +0800

    finished exercise

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, 2nd, 3rd = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", 2nd
+print "Your third variable is:", 3rd
